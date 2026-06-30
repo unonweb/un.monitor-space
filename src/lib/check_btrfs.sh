9cf181f6e4b581f69be3477d07d164b97511ec80
unonweb <unonweb@posteo.de>
2026-06-30 02:37:49 +0200

    add cache to btrfs fctn

--- src/lib/check_btrfs.sh
+++ src/lib/check_btrfs.sh
@@ -2,13 +2,19 @@
 # ========
 # - root
 # - THRESHOLD_PERCENT_FREE
+# - CACHE_FILE
+# - CACHE_TTL_HOURS
+# - ALERT_MSG
 
 function check_btrfs {
+
 	# Loop through /proc/mounts to find all 'btrfs' filesystems
 	# We use a while loop to read line-by-line using Bash internals
 	while read -r device mount_point fs_type options _; do
+		
 		# Filter for btrfs type only
     	if [[ "${fs_type}" == "btrfs" ]]; then
+		
 			log "<6> Checking Btrfs filesystem mounted at: ${mount_point}"
 			# Query btrfs for data in Gigabytes (requires root/sudo)
         	btrfs_output=$(btrfs filesystem usage -g "${mount_point}" 2>/dev/null)
@@ -31,14 +37,25 @@ function check_btrfs {
 				log "<6> Total Size: ${total_size} GiB"
 				log "<6> Free Space: ${free_space} GiB (${pct_free}% free)"
 				
-				# Comparison
+				# THRESHOLD
 				if (( pct_free < THRESHOLD_PERCENT_FREE )); then
-					echo "--> ALERT: Free space (${pct_free}%) is below threshold (${THRESHOLD_PERCENT_FREE}%)!"
+
+					# CACHE
+					if [[ -f "${CACHE_FILE}" ]] && grep --quiet --fixed-strings "|${mount_point}" "${CACHE_FILE}"; then
+						# mount_point found in cache, skip alerting
+						log "<7> Skipping alert for '${mount_point}' (already alerted within past ${CACHE_TTL_HOURS} hours)."
+					else
+						# ALERT msg
+						ALERT_MSG+="${msg}\n"
+						# Log the alert to the cache with the current epoch timestamp
+						echo "$(date +%s)|${mount_point}" >> "${CACHE_FILE}"
+					fi
+					log "<4> ALERT: Free space at ${mount_point} is ${pct_free}% and below threshold (${THRESHOLD_PERCENT_FREE}%)!"
 				else
-					echo "--> Space status: OK"
+					log "<6> Space of status of ${mount_point}: OK"
 				fi
 			else
-				echo "--> Error: Could not calculate space metrics for ${mount_point}"
+				log "<3> Error: Could not calculate space metrics for ${mount_point}"
 			fi
 		fi
 	done < /proc/mounts
